f5e858efedadfb907d1f8a47aa714ac975412aa7
pietervdvn <pietervdvn@posteo.net>
2022-03-07 21:51:38 +0100

    More work on the generator

--- generator/Generator.ts
+++ generator/Generator.ts
@@ -13,6 +13,12 @@ const black = {
     style: "fill:none;stroke-width:1"
 }
 
+const green = {
+    stroke: "#00ff00",
+    "stroke-width": "1mm",
+    style: "fill:none;stroke-width:1"
+}
+
 
 class Vect {
 
@@ -45,15 +51,19 @@ class Vect {
         return " M" + this.x + " " + this.y + " L" + to.x + " " + to.y
     }
 
-    public dist(to: Vect): number {
+    public dist(to?: Vect): number {
         return Math.sqrt(
-            Math.pow(this.x - to.x, 2) +
-            Math.pow(this.y - to.y, 2)
+            Math.pow(this.x - (to?.x ?? 0), 2) +
+            Math.pow(this.y - (to?.y ?? 0), 2)
         )
     }
 
     public translate(): Vect {
         return new Vect(this.y, this.x)
+    }
+
+    public normalize(): Vect {
+        return this.mul(1 / this.dist())
     }
 
     public rotate(degrees: number): Vect {
@@ -65,12 +75,15 @@ class Vect {
     }
 }
 
-function circle(cx: number, cy: number, radius: number): any {
+function circle(center: Vect, radius: number): any {
+    if (radius <= 0) {
+        return undefined
+    }
     return {
         circle:
             {
                 $: {
-                    cx, cy, r: radius,
+                    cx: center.x, cy: center.y, r: radius,
                     ...cutStyle
                 }
             }
@@ -82,7 +95,7 @@ function circle(cx: number, cy: number, radius: number): any {
  * @param n
  * @param length
  */
-function radiusForRegularoid(n:number, length : number){
+function radiusForRegularoid(n: number, length: number) {
     /*
     
     x
@@ -102,13 +115,13 @@ function radiusForRegularoid(n:number, length : number){
     Thus: radius = length / sin(180° / n) * 2
     
      */
-    return length / (Math.sin(Math.PI / n) * 2 )
+    return length / (Math.sin(Math.PI / n) * 2)
 }
 
 function regularoid(center: Vect, n: number, radius: number): Vect[] {
 
-    if(radius < 1){
-        throw "This is a very small radius: "+radius
+    if (radius < 1) {
+        throw "This is a very small radius: " + radius
     }
     const points = []
 
@@ -143,26 +156,26 @@ function line(coordinates: Vect[], style = cutStyle) {
     }
 }
 
-function rect(c1: Vect, c2: Vect, width: number): Vect[]{
+function rect(c1: Vect, c2: Vect, width: number): Vect[] {
     const m = c2.sub(c1).rotate(-90).mul(width / 2 / c2.dist(c1))
     const m0 = c2.sub(c1).rotate(90).mul(width / 2 / c2.dist(c1))
 
     return [c1.add(m), c2.add(m), c2.add(m0), c1.add(m0), c1.add(m)]
 }
 
-function teethStarts(start: Vect, end: Vect, width: number, teethWidth: number, nogap: boolean = false): [Vect, Vect][]{
-    const vects : [Vect, Vect][] = []
+function teethStarts(start: Vect, end: Vect, width: number, teethWidth: number, nogap: boolean = false): [Vect, Vect][] {
+    const vects: [Vect, Vect][] = []
     const d = start.dist(end)
     let iterations = Math.floor(d / (width + teethWidth));
 
-    if(nogap){
+    if (nogap) {
         iterations++
     }
     const m = end.sub(start).mul(1 / d);
 
     for (let j = 0; j < iterations; j++) {
         let segmentStart = start.add(m.mul(j * (width + teethWidth) + teethWidth))
-        let segmentStop = start.add(m.mul((j+1) * (width + teethWidth) ));
+        let segmentStop = start.add(m.mul((j + 1) * (width + teethWidth)));
         vects.push([segmentStart, segmentStop])
     }
 
@@ -171,24 +184,24 @@ function teethStarts(start: Vect, end: Vect, width: number, teethWidth: number, 
 
 function teeth(coordinates: Vect[], configs: { degrees?: number, reverse?: boolean | false, nogap?: boolean | false, width: number, depth: number, teethWidth?: number }[]): Vect[] {
     const allVects = []
-    if(configs.length !== coordinates.length - 1){
-        throw "Invalid number of configs, expected "+(coordinates.length - 1)+ " but got "+configs.length
+    if (configs.length !== coordinates.length - 1) {
+        throw "Invalid number of configs, expected " + (coordinates.length - 1) + " but got " + configs.length
     }
-    
+
     for (let i = 1; i < coordinates.length; i++) {
         let start = coordinates[i - 1]
         let c = coordinates[i]
         let isRev = false;
-        const config = configs[i-1]
-        if(config === undefined){
-            if(i == 1){
+        const config = configs[i - 1]
+        if (config === undefined) {
+            if (i == 1) {
                 allVects.push(start)
             }
-            
+
             allVects.push(c)
             continue
         }
-        
+
         if (config?.reverse) {
             const x = c
             c = start
@@ -204,7 +217,7 @@ function teeth(coordinates: Vect[], configs: { degrees?: number, reverse?: boole
         const depth = config.depth
         let iterations = Math.floor(d / (width + teethWidth));
 
-        if(config?.nogap){
+        if (config?.nogap) {
             iterations++
         }
         const m = c.sub(start).mul(1 / d);
@@ -237,17 +250,17 @@ function teeth(coordinates: Vect[], configs: { degrees?: number, reverse?: boole
             vects.push(c)
             vects.reverse()
         }
-        if(config?.nogap){
-            
+        if (config?.nogap) {
+
             vects = vects.map(v => v.add(m.mul(-width)))
             vects.splice(vects.length - 1, 4)
 
         }
-        
-        if(config?.nogap && isRev){
+
+        if (config?.nogap && isRev) {
             vects.splice(0, 2)
         }
-        
+
         allVects.push(...vects)
     }
     return allVects
@@ -255,45 +268,204 @@ function teeth(coordinates: Vect[], configs: { degrees?: number, reverse?: boole
 }
 
 
-const frontplate_top_length = (150 - 28.5 * 2)
-const frontplate_bottom_length = (150 - 21 * 2)
+class PiltoverLantern {
 
-function frontPlate(): Vect[] {
-    const front_plate_outline_half: [number, number][] = [
-        [21, 215],
-        [0, 57],
-        [28.5, 0],
-        [150 - 28.5, 0],
-        [150, 57],
-        [150 - 21, 215],
-        [21, 215],
-    ]
-    return front_plate_outline_half.map(([x, y]) => new Vect(x, y))
-}
+    private readonly svg
 
-function bottomTriangle(): Vect[] {
-    // horizontal line, right, left
-    const front_plate_outline_half: [number, number][] = [
-        [-28.5, 0],
-        [28.5, 0],
-        [0, 57],
-        [-28.5, 0],
-    ]
-    return front_plate_outline_half.map(([x, y]) => new Vect(28.5 + x, y))
-}
+    private readonly plateWidth
+    private readonly width = 3.1;
+    private readonly teethWidth = 2.9;
+    private readonly depth = 3.0;
+    private readonly crownConnectWidth = 5.0
+    private readonly smallHoleSizeRadius = 7.5
+    private centralCircleRadius: number;
 
-
-function main(): void {
-
-    const w = 300
-    const h = 400
-    const svg = {
-        $: {
-            width: w + "mm", height: h + "mm", version: "1.1", viewBox: "0 0 " + w + " " + h
+    constructor(canvasWidth: number, canvasHeight: number, centralCircleRadius: number, plateWidth: number = 3) {
+        this.centralCircleRadius = centralCircleRadius;
+        this.plateWidth = plateWidth
+        this.svg = {
+            $: {
+                width: canvasWidth + "mm",
+                height: canvasHeight + "mm",
+                version: "1.1",
+                viewBox: "0 0 " + canvasWidth + " " + canvasHeight
+            }
         }
+
+        const baseplateLength = 150 - 21 * 2
+
+        const lbottom = this.addBaseplate(new Vect(0, 0), baseplateLength)
+        this.addCrownPlate(50, lbottom, new Vect(0, 0))
+        this.addEndPlate(lbottom, new Vect(0, 0))
+        const ltop = this.addTopPlate(new Vect(0, 0), 93)
+        this.addCrownPlate(50, ltop, new Vect(0, 0))
+        this.addEndPlate(ltop, new Vect(0, 0))
+
+
+        this.addTriangles(new Vect(0, 0))
+        this.addTopRect(new Vect(0, 0))
+        this.addMainPlate(new Vect(0, 0))
+
     }
 
-    function add(item: any): void {
+    public asXml(): string {
+        const builder = new xml2js.Builder();
+        return builder.buildObject({svg: this.svg});
+    }
+
+    private addEndPlate(l, offset: Vect) {
+        const r = radiusForRegularoid(5, l)
+        this.add(line(regularoid(new Vect(r, r).add(offset), 5, r)))
+        this.add(circle(new Vect(r, r).add(offset), this.smallHoleSizeRadius))
+    }
+
+    private addCrownPlate(h: number, w: number, offset: Vect) {
+        const outline = [
+            [5, 0],
+            [w - 5, 0],
+            [w, 5],
+            [w, h + this.plateWidth],
+            [w - this.crownConnectWidth, h + this.plateWidth],
+            [w - this.crownConnectWidth, h],
+            [this.crownConnectWidth, h],
+            [this.crownConnectWidth, h + this.plateWidth],
+            [0, h + this.plateWidth],
+            [0, 5],
+            [5, 0]
+        ]
+        this.add(line(outline.map(([x, y]) => new Vect(x, y).add(offset))))
+    }
+
+    private addTopPlate(offset: Vect,
+                        top_length: number) {
+        const baseplate_center = offset.addxy(top_length, top_length)
+        const topplate_outer = regularoid(baseplate_center, 5, radiusForRegularoid(5, top_length))
+        const baseplate_crown = regularoid(baseplate_center, 5, radiusForRegularoid(5, top_length - 10))
+        const config = {
+            width: this.width, teethWidth: this.teethWidth, depth: this.plateWidth * 1.5,
+            reverse: false,
+            noGap: true
+        }
+        this.add(line(teeth(topplate_outer, [config, config, config, config, config])))
+        this.add(circle(baseplate_center, this.centralCircleRadius))
+        const crownsize = Math.sin(Math.PI * 2 / 5)
+        for (let i = 0; i < 5; i++) {
+            const crown_start = baseplate_crown[i]
+            const crown_end = baseplate_crown[i + 1]
+            const rot = crown_end.sub(crown_start).normalize()
+            this.add(line(rect(crown_start.add(rot.mul(crownsize)), crown_start.add(rot.mul(crownsize + this.crownConnectWidth)), this.plateWidth)))
+            this.add(line(rect(crown_end.sub(rot.mul(crownsize)), crown_end.sub(rot.mul(crownsize + this.crownConnectWidth)), this.plateWidth)))
+        }
+
+        const crown_start = baseplate_crown[0]
+        const crown_end = baseplate_crown[1]
+        const rot = crown_end.sub(crown_start).normalize()
+        const start = crown_start.add(rot.mul(crownsize))
+        const end = crown_end.sub(rot.mul(crownsize))
+        return start.dist(end)
+
+    }
+
+    /**
+     *
+     * @param offset
+     * @param bottom_length
+     * @private
+     * @returns the width that the crown plate should be
+     */
+    private addBaseplate(
+        offset: Vect,
+        bottom_length: number
+    ): number {
+        const baseplate_center = offset.addxy(bottom_length, bottom_length)
+        const baseplate_outer = regularoid(baseplate_center, 5, radiusForRegularoid(5, bottom_length + 5))
+        const baseplate_connect = regularoid(baseplate_center, 5, radiusForRegularoid(5, bottom_length + 0.1)) // + 0.1, as rounding ate one hole
+        const baseplate_crown = regularoid(baseplate_center, 5, radiusForRegularoid(5, bottom_length - 10))
+        this.add(line(baseplate_outer, black))
+        this.add(line(baseplate_connect, black))
+        this.add(line(baseplate_crown, black))
+        this.add(circle(baseplate_center, this.centralCircleRadius))
+        const crownsize = Math.sin(Math.PI * 2 / 5)
+        for (let i = 0; i < 5; i++) {
+            const start = baseplate_connect[i]
+            const end = baseplate_connect[i + 1]
+            let teeth = teethStarts(start, end, this.width, this.teethWidth)
+
+            for (const [toothStart, toothEnd] of teeth) {
+                this.add(line(rect(toothStart, toothEnd, this.plateWidth * 1.4)))
+            }
+
+            const crown_start = baseplate_crown[i]
+            const crown_end = baseplate_crown[i + 1]
+            const rot = crown_end.sub(crown_start).normalize()
+            this.add(line(rect(crown_start.add(rot.mul(crownsize)), crown_start.add(rot.mul(crownsize + this.crownConnectWidth)), this.plateWidth)))
+            this.add(line(rect(crown_end.sub(rot.mul(crownsize)), crown_end.sub(rot.mul(crownsize + this.crownConnectWidth)), this.plateWidth)))
+        }
+
+        const crown_start = baseplate_crown[0]
+        const crown_end = baseplate_crown[1]
+        const rot = crown_end.sub(crown_start).normalize()
+        const start = crown_start.add(rot.mul(crownsize))
+        const end = crown_end.sub(rot.mul(crownsize))
+        return start.dist(end)
+    }
+
+    private addTriangles(offset: Vect) {
+
+        const bottomTriangle: Vect[] = [
+            [-28.5, 0],
+            [28.5, 0],
+            [0, 57],
+            [-28.5, 0],
+        ].map(([x, y]) => new Vect(28.5 + x, y))
+
+        this.add(line(teeth(
+            bottomTriangle,
+            [
+                {
+                    reverse: true,
+                    nogap: true,
+                    width: this.width, teethWidth: this.teethWidth, depth: this.depth
+                }, // horiz
+                undefined, undefined
+            ]
+        ).map(v => v.rotate(180).add(offset).addxy(57, 57))))
+
+        this.add(line(teeth(
+            bottomTriangle,
+            [
+                {
+                    width: this.width, teethWidth: this.teethWidth, depth: this.depth
+                }, // horiz
+                undefined, undefined
+            ]
+        ).map(v => v.addxy(0, 60).add(offset))))
+    }
+
+    private addTopRect(offset: Vect) {
+        const topRect = [new Vect(0, 0), new Vect(0, 66), new Vect(93, 66), new Vect(93, 0), new Vect(0, 0)]
+        const teethedTopRect = teeth(topRect, [
+            undefined,
+            {
+                width: this.width,
+                teethWidth: this.teethWidth,
+                depth: this.depth,
+            },
+            undefined,
+            {
+                width: this.width,
+                teethWidth: this.teethWidth,
+                depth: this.depth,
+            }
+        ])
+        this.add(line(teethedTopRect.map(v => v.add(offset))))
+    }
+
+    private add(item: any): void {
+        if (item === undefined) {
+            return
+        }
+
         for (const key of Object.keys(item)) {
             let value = item[key]
             let arr: any[];
@@ -302,107 +474,52 @@ function main(): void {
             } else {
                 arr = [value]
             }
-            if (svg[key] !== undefined) {
-                svg[key].push(...arr)
+            if (this.svg[key] !== undefined) {
+                this.svg[key].push(...arr)
             } else {
-                svg[key] = arr
+                this.svg[key] = arr
             }
         }
     }
 
-    //   add(circle(25,80,10))
-    //  add(teeth([new Vect(0,50), new Vect(500,50),new Vect(500,500)], 20, 20))
-    // add(teeth([new Vect(500,50), new Vect(0,50),new Vect(500,500)], 20, 20))
-   // add(line(frontPlate().map(v => v.addxy(5, 5)), black))
-    const plateWidth = 3.0
-    const width = 3.1;
-    const teethWidth= 2.9;
-    const depth = 3.0;
-   
-    add(line(teeth(frontPlate().map(v => v.addxy(25, 70)), [
-        undefined,
-        undefined,
-        {
-            // Top
-            width,
-            teethWidth,
-            depth
-        },
-        undefined,
-        undefined,
-        {
-            // Bottom
-            width,
-            teethWidth,
-            depth
-        },
-    ])))
+    private addMainPlate(offset: Vect) {
 
-    add(line(teeth(
-        bottomTriangle(),
-        [
+        const frontPlate = [
+            [21, 215],
+            [0, 57],
+            [28.5, 0],
+            [150 - 28.5, 0],
+            [150, 57],
+            [150 - 21, 215],
+            [21, 215],
+        ].map(([x, y]) => new Vect(x, y + 5).add(offset))
+
+        this.add(line(teeth(frontPlate, [
+            undefined,
+            undefined,
             {
-                width, teethWidth, depth
-            }, // horiz
-            undefined, undefined
-        ]
-    ).map(v => v.addxy(150,70))))//*/
-
-    add(line(teeth(
-        bottomTriangle(),
-        [
+                // Top
+                width: this.width,
+                teethWidth: this.teethWidth,
+                depth: this.depth
+            },
+            undefined,
+            undefined,
             {
-                reverse: true,
-                nogap: true,
-                width, teethWidth, depth
-            }, // horiz
-            undefined, undefined
-        ]
-    ).map(v => v.addxy(150,25))))//*/
-    
-    const topRect = [new Vect(0,0), new Vect(0,66),  new Vect(93,66), new Vect(93,0), new Vect(0,0)]
-    const teethedTopRect = teeth(topRect, [
-        undefined,
-        {
-            width,
-            teethWidth,
-            depth
-        },
-        undefined,
-        {
-            width,
-            teethWidth,
-            depth
-        }
-    ])
-    add(line(topRect .map(v => v.addxy(54, 0)), black))
-    add(line(teethedTopRect.map(v => v.addxy(54,0))))
-
-        /*
-    {
-    const baseplate_center = new Vect(75,140)
-    const baseplate_outer = regularoid(baseplate_center, 5, radiusForRegularoid(5, frontplate_bottom_length + 5))
-    const baseplate_connect = regularoid(baseplate_center, 5, radiusForRegularoid(5, frontplate_bottom_length ))
-    add(line(baseplate_outer, black))
-    add(line(baseplate_connect, black))
-    for (let i = 0; i < baseplate_connect.length - 1; i++) {
-        const start = baseplate_connect[i]
-        const end = baseplate_connect[i+1]
-        const teeth = teethStarts(start, end, width, teethWidth)
-        for (const [toothStart, toothEnd] of teeth) {
-            add(line(rect(toothStart, toothEnd, plateWidth * 1.4)))
-        }
+                // Bottom
+                width: this.width,
+                teethWidth: this.teethWidth,
+                depth: this.depth
+            },
+        ])))
     }
-    }//*/
+}
 
 
-    const builder = new xml2js.Builder();
-    const xml = builder.buildObject({svg});
-    writeFileSync("Generated.svg", xml)
-
-    console.log("Done "+new Date().toISOString())
-
-
+function main(): void {
+    const sketch = new PiltoverLantern(300, 400, 60, 3)
+    writeFileSync("Generated.svg", sketch.asXml())
+    console.log("Done " + new Date().toISOString())
 }
 
 main()
